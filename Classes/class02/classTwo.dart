28952f975ca50e64d66d54507f66fe4407e6eb58
Mohsin0700 <muhammad786moshin024@gmail.com>
2023-07-15 00:37:45 +0500

    files updated

---
+++ Classes/class02/classTwo.dart
@@ -0,0 +1,89 @@
+void main() {
+  // String abc = 'abc';
+
+  // int num1 = 20;
+
+  // double num2 = 12.5;
+
+  // num num3 = 550;
+
+  // num num4 = 6.666;
+
+  // bool condition = false;
+
+  // bool secondCondition = true;
+
+  // print('$abc, $num1, $num2, $num3, $num4, $condition, $secondCondition');
+
+  // var result = num1 > num3;
+
+  // print(result);
+// *******************************************************************************
+  // Arithmetic operators
+
+  // num num1 = 5;
+
+  // num num2 = 7;
+
+  // num result = num1 + num2;
+  // print(result);
+
+  // num result2 = num2 - num1;
+  // print(result2);
+
+  // num result3 = num2 * num1;
+  // print(result3);
+
+  // num result4 = 20 / num1;
+  // print(result4);
+
+  // num result5 = 20 ~/ num1;
+  // print(result5);
+
+  // num result6 = 21 % num1;
+  // print(result6);
+  // ***************************************************************************
+
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  // Equality and relational operators
+
+  // int num1 = 10;
+  // int num2 = 5;
+  // int num3 = 10;
+
+  // print(num1 > num2);
+  // print(num1 < num2);
+  // print(num1 >= num3);
+  // print(num1 <= num3);
+  // print(num1 != num2);
+
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  // Logical Operators and if and else
+
+  // if (num1 > num2 && num3 > num2) {
+  //   print('Perfect');
+  // } else {
+  //   print('Imperfect');
+  // }
+
+  int age = 18;
+  // int age = 17;
+  // String city = 'karachi';
+  String city = 'hyderabad';
+
+  // if (age >= 18 && city == 'karachi') {
+  //   print('you are eligible');
+  // } else {
+  //   print("you are not eligible");
+  // }
+
+  if (age >= 18 || city == 'karachi') {
+    print('you are eligible');
+  } else {
+    print("you are not eligible");
+  }
+
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+}
